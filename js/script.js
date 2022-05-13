22a95e79dd56d046ad54c92e05a8214c1255b505
Nathan Araujo <99274283+nathan-araujo@users.noreply.github.com>
2022-05-13 14:28:36 +0000

    added comments

--- js/script.js
+++ js/script.js
@@ -20,46 +20,59 @@ function order () {
 
 	//declare constants
 	const HST = 0.13
-
+	const SMALL_PRICE = 7.99
+	const MEDIUM_PRICE = 12.49
+	const LARGE_PRICE = 18.99
+	const BACON_PRICE = 1
+	const PULLEDPORK_PRICE = 3
+	const BUTTERCHICKEN_PRICE = 3
+	const TATERTOTS_PRICE = 1
+	const DRINK_PRICE = 2
 	//input
 	let select = document.getElementById('size');
 	let size = select.options[select.selectedIndex].value;
 
-	//IF Statements for sizes
+	//IF small size is selected then set small price
 	if (size == "small") {
-		sizes = 7.99
+		sizes = SMALL_PRICE
 	}
+	//IF medium size is selected then set medium price
 	else if (size == "medium") {
-		sizes = 12.49
+		sizes = MEDIUM_PRICE
 	}
+	//IF large size is selected then set large price
 	else if (size == "large"){
-		sizes = 18.99
+		sizes = LARGE_PRICE
 	}
 
-	//IF statements for toppings
+	//IF bacon is selected then set bacon price
 	if (document.getElementById('bacon').checked) {
-		bacon = 1
+		bacon = BACON_PRICE
 	}
+	//IF pulled pork is selected then set bacon price
 	if (document.getElementById('pulledPork').checked) {
-		pulledPork = 3
+		pulledPork = PULLEDPORK_PRICE
 	}
+	//IF butter chicken is selected then set bacon price
 	if (document.getElementById('butterChicken').checked) {
-		butterChicken = 3
+		butterChicken = BUTTERCHICKEN_PRICE
 	}
+	//IF tater tots is selected then set bacon price
 	if (document.getElementById('taterTots').checked) {
-		taterTots = 1
+		taterTots = TATERTOTS_PRICE
 	}
 
-	//IF statements for drinks[]
+	//IF drinks is selected then set drink price
 	if (document.getElementById('drink-yes').checked) {
-		drink = 2
+		drink = DRINK_PRICE
 	}
 
-	//subtotal
+	//Calculations for subtotal, tax and total
 	let subtotal = (sizes + bacon + pulledPork + butterChicken + taterTots + drink)
 	let tax = subtotal * HST
 	let total = subtotal + tax
-	
+
+	//Display results
 	document.getElementById('display').innerHTML = "Your subtotal is $" + subtotal.toFixed(2) + " and your tax is $" + tax.toFixed(2) + " which means your total is $" + total.toFixed(2) + "."
 }
 	
